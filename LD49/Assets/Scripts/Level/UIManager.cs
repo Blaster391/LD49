2a41bdce659967c6a4b910f3d3e382309437bf34
Robert Arnold <robertarnold391@gmail.com>
2021-10-02 15:37:43 +0100

    UI

---
+++ LD49/Assets/Scripts/Level/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI m_timer;
+
+    [SerializeField]
+    private TextMeshProUGUI m_gameOver;
+
+    [SerializeField]
+    private TextMeshProUGUI m_levelComplete;
+
+    private LevelManager m_levelManager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_timer.gameObject.SetActive(false);
+        m_gameOver.gameObject.SetActive(false);
+        m_levelComplete.gameObject.SetActive(false);
+
+        m_levelManager = GetComponentInParent<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer.gameObject.SetActive(false);
+        m_gameOver.gameObject.SetActive(false);
+        m_levelComplete.gameObject.SetActive(false);
+
+        if (m_levelManager.LevelIsComplete())
+        {
+            m_levelComplete.gameObject.SetActive(true);
+        }
+        else if(m_levelManager.LevelIsFailed())
+        {
+            m_gameOver.gameObject.SetActive(true);
+        }
+        else
+        {
+            m_timer.gameObject.SetActive(true);
+
+            m_timer.text = $"{Mathf.RoundToInt(m_levelManager.TimeLeft())}";
+        }
+    }
+}
