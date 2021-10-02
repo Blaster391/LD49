83eba72198521c37d5b7adee79585fb8741e1384
Robert Arnold <robertarnold391@gmail.com>
2021-10-02 12:10:54 +0100

    Moving stuff

---
+++ LD49/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Physics2D.IgnoreLayerCollision(0, 8);
+        Physics2D.IgnoreLayerCollision(8, 8);
+    }
+
+    private Grabbable m_grabbable = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            int layer = 8;
+            int layerMask = (1 << layer);
+            layerMask = ~layerMask;
+
+            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D result = Physics2D.OverlapPoint(mouseWorld);
+
+            m_grabbable = result?.transform?.gameObject?.GetComponent<Grabbable>();
+            if(m_grabbable != null)
+            {
+                m_grabbable.StartGrab();
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0) && m_grabbable != null)
+        {
+            m_grabbable.EndGrab();
+            m_grabbable = null;
+        }
+
+    }
+}
--- LD49/Assets/Scripts/Grabbable.cs
+++ LD49/Assets/Scripts/Grabbable.cs
@@ -2,17 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Runtime.InteropServices;
+
 public class Grabbable : MonoBehaviour
 {
+
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int X, int Y);
+
+    private bool m_grabbed = false;
+    private GameObject m_parent;
+    private Rigidbody2D m_parentRigidbody;
+    private Vector3 m_clickPoint = Vector3.zero;
+    private Vector2 m_cachedMousePosition = Vector2.zero;
+
+    [SerializeField]
+    private float m_movementForce = 10000.0f;
+
+    [SerializeField]
+    private float m_rotateForce = 5000.0f;
+
+    [SerializeField]
+    private float m_angularDrag = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_parent = transform.parent.gameObject;
+        m_parentRigidbody = m_parent.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(m_grabbed)
+        {
+            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 forceDirection = mouseWorld - m_parent.transform.position;
+            m_parentRigidbody.velocity = Vector3.zero;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                m_cachedMousePosition = Input.mousePosition;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+            if(Input.GetMouseButtonUp(1))
+            {
+                SetCursorPos((int)m_cachedMousePosition.x, (int)m_cachedMousePosition.y);
+
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            if (Input.GetMouseButton(1))
+            {
+                m_parentRigidbody.AddTorque(Input.GetAxis("Rotate") * m_rotateForce);
+            }
+            else
+            {
+                m_parentRigidbody.AddTorque(Input.mouseScrollDelta.y * m_rotateForce);
+
+                Cursor.lockState = CursorLockMode.None;
+
+                forceDirection.Normalize();
+
+                m_parentRigidbody.AddForce(forceDirection * m_movementForce);
+            }
+        }
+    }
+
+    public void StartGrab()
+    {
+        Debug.Log(m_parent.name + " Grabbed");
+
+        m_grabbed = true;
+        m_parentRigidbody.gravityScale = 0.0f;
+
+        m_parentRigidbody.angularDrag = m_angularDrag;
+    }
+
+    public void EndGrab()
+    {
+        Cursor.lockState = CursorLockMode.None;
+
+        Debug.Log(m_parent.name + " Dropped");
+
+        m_grabbed = false;
+        m_parentRigidbody.gravityScale = 1.0f;
+
+        m_parentRigidbody.angularDrag = 0.0f;
     }
 }
