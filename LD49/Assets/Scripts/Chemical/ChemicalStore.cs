20ceb58b1ddddd81bff6160231b406b052ed9d89
Robert Arnold <robertarnold391@gmail.com>
2021-10-02 14:30:34 +0100

    Core game looping

---
+++ LD49/Assets/Scripts/Chemical/ChemicalStore.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemicalStore : MonoBehaviour
+{
+    [SerializeField] 
+    private ChemicalData m_targetChemicalType = null;
+    [SerializeField]
+    private uint m_targetChemicalCount = 0;
+
+    private List<IChemicalState> m_containedChemicals = new List<IChemicalState>();
+    private bool m_complete = false;
+
+    private LevelManager m_levelManager = null;
+    private void Start()
+    {
+        GetComponentInParent<LevelManager>().RegisterStore(this);
+    }
+
+
+    void Update()
+    {
+        uint correctCount = 0;
+
+        if(m_containedChemicals.Count >= m_targetChemicalCount)
+        {
+            foreach(var chemical in m_containedChemicals)
+            {
+                if(chemical.State == m_targetChemicalType)
+                {
+                    correctCount++;
+                }
+            }
+        }
+
+        m_complete = correctCount >= m_targetChemicalCount;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IChemicalState chemical = collision.GetComponent<IChemicalState>();
+        if(chemical != null)
+        {
+            m_containedChemicals.Add(chemical);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IChemicalState chemical = collision.GetComponent<IChemicalState>();
+        if (chemical != null)
+        {
+            m_containedChemicals.Remove(chemical);
+        }
+    }
+
+    public bool IsComplete()
+    {
+        return m_complete;
+    }
+
+}
---
+++ LD49/Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private string m_nextLevel;
+
+    private List<ChemicalStore> m_stores = new List<ChemicalStore>();
+    private bool m_complete = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) // TODO maybe R bad?
+        {
+            RestartLevel();
+        }
+
+        if(!m_complete)
+        {
+            m_complete = m_stores.TrueForAll(x => x.IsComplete());
+        }
+
+        if(m_complete)
+        {
+            Debug.Log("u r winner");
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                NextLevel();
+            }
+
+        }
+    }
+
+    public void RegisterStore(ChemicalStore _store)
+    {
+        m_stores.Add(_store);
+    }
+
+    public bool LevelIsComplete()
+    {
+        return m_complete;
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(m_nextLevel, LoadSceneMode.Single);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+}
