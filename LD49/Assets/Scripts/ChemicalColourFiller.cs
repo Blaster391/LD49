dc2fb125c0e5710feea2fe5fca678ce282b89de0
Robert Arnold <robertarnold391@gmail.com>
2021-10-04 18:56:10 +0100

    Chemical sign for tap

---
+++ LD49/Assets/Scripts/ChemicalColourFiller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemicalColourFiller : MonoBehaviour
+{
+    [SerializeField]
+    private ChemicalData m_chemical;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = m_chemical.Color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
